30a4bde4c3d858feb4296ede8f419aa968bd8acc
Majd Sabah <msabah@winfooz.com>
2018-10-07 15:31:22 +0300

    Initial Commit

---
+++ SwiftyVIN/SwiftyVIN.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyVIN.h
+//  SwiftyVIN
+//
+//  Created by Majd Sabah on 10/7/18.
+//  Copyright © 2018 Majd Sabah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyVIN.
+FOUNDATION_EXPORT double SwiftyVINVersionNumber;
+
+//! Project version string for SwiftyVIN.
+FOUNDATION_EXPORT const unsigned char SwiftyVINVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyVIN/PublicHeader.h>
+
+
---
+++ SwiftyVINTests/SwiftyVINTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftyVINTests.swift
+//  SwiftyVINTests
+//
+//  Created by Majd Sabah on 10/7/18.
+//  Copyright © 2018 Majd Sabah. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftyVIN
+
+class SwiftyVINTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
